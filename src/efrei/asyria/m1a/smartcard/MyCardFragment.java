5976c793f39a1fc51d85025591535746d31ca692
Samantha ALWIS <samantha.alwis.k@gmail.com>
2014-05-08 16:26:41 +0200

    added my card info

--- src/efrei/asyria/m1a/smartcard/MyCardFragment.java
+++ src/efrei/asyria/m1a/smartcard/MyCardFragment.java
@@ -1,20 +1,60 @@
 package efrei.asyria.m1a.smartcard;
 
+import java.util.HashMap;
+
+import efrei.asyria.m1a.session.SessionLogin;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MyCardFragment extends Fragment {
+	private TextView tvName;
+	private TextView tvEmail;
+
+	private TextView tvPhone1;
+	private TextView tvPhone2;
+
+	private TextView tvJob;
+	private TextView tvCname;
+
+	private TextView tvCcity;
+	private TextView tvCcountry;
+	private TextView tvCadress;
+
+
+    SessionLogin session;
 	public MyCardFragment(){}
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-  
-        View rootView = inflater.inflate(R.layout.fragment_mycard, container, false);
-          
-        return rootView;
+    	View rootView = inflater.inflate(R.layout.fragment_mycard, container, false);
+
+    	session = new SessionLogin(container.getContext());
+        HashMap<String, String> user = session.getUserInfo();
+		tvName = (TextView) rootView.findViewById(R.id.name);
+		tvEmail = (TextView) rootView.findViewById(R.id.email);
+		tvPhone1 = (TextView) rootView.findViewById(R.id.phoneF);
+		tvPhone2 = (TextView) rootView.findViewById(R.id.phoneM);
+		//tvJob = (TextView) rootView.findViewById(R.id.tvJob);
+		//tvCname = (TextView) rootView.findViewById(R.id.tvCname);
+		tvCadress = (TextView) rootView.findViewById(R.id.adress);
+		tvCcity = (TextView) rootView.findViewById(R.id.cpCity);
+
+		System.out.println(user.get(SessionLogin.KEY_USERNAME));
+		System.out.println(user.get(SessionLogin.KEY_EMAIL));
+
+		tvName.setText(user.get(SessionLogin.KEY_NAME));
+		tvEmail.setText(user.get(SessionLogin.KEY_EMAIL));
+		tvPhone1.setText(user.get(SessionLogin.KEY_PHONE1));
+		tvPhone2.setText(user.get(SessionLogin.KEY_PHONE2));
+		//tvCname.setText(user.get(SessionLogin.KEY_CNAME));
+		tvCadress.setText(user.get(SessionLogin.KEY_CADRESS));
+		tvCcity.setText(user.get(SessionLogin.KEY_CCITY));
+		
+		return rootView;
     }
 }
